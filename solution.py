7084037f4f5b2e2015d2e228023919dca4459daf
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-06 12:17:14 +0500

    не умножать на 2

--- solution.py
+++ solution.py
@@ -15,10 +15,15 @@ def solution(p: float, x: np.array) -> tuple:
     loc = x.mean()
     var = sum([(i - loc) ** 2 for i in x])
     scale = np.sqrt(var / (len(x)))
-    return 2 * loc - 2 * (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))), \
-           2 * loc + 2 * (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)))
-
+    #return 2 * loc - 2 * (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))), \
+    #       2 * loc + 2 * (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)))
+    return loc - (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))), \
+           loc + (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)))
+    #return scale
+    #return norm.ppf(alpha/2), norm.ppf(1 - alpha/2)
+    #return norm.ppf(1 - alpha/2)
+    #return loc
 
 #if __name__ == '__main__':
-##    m = np.array([12, 24, 56, 78])
+##    m = np.array([0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.96, 0.97, 0.98, 0.99])
 #    print(solution(0.95, m))

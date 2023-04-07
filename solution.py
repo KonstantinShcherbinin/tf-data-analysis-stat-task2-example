2556bbc2670dc0988dbebc062b95d6576efaa942
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-07 09:53:51 +0500

    коэф 3

--- solution.py
+++ solution.py
@@ -14,15 +14,16 @@ def solution(p: float, x: np.array) -> tuple:
     loc = x.mean()
     var = sum([(i - loc) ** 2 for i in x])
     scale = np.sqrt(var / (len(x)))
-    return 2 * loc - 2 * (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))), \
-           2 * loc + 2 * (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)))
+    return 2 * loc - 3 * (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))), \
+           2 * loc + 3 * (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)))
     #return loc + (scale * norm.ppf(alpha / 2) / np.sqrt(len(x))), \
     #       loc + (scale * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)))
     #return scale
     #return norm.ppf(alpha/2), norm.ppf(1 - alpha/2)
     #return norm.ppf(1 - alpha/2)
     #return loc
+    #return loc / (1 - alpha), loc/ alpha
 
 #if __name__ == '__main__':
-#    m = np.array([0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.96, 0.97, 0.98, 0.99])
-#    print(solution(0.99, m))
+##    m = np.array([0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.96, 0.97, 0.98, 0.99])
+#    print(solution(0.9, m))
